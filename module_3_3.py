d54d4af05a483654b9ccf5d89780641d1316702a
LeilaAimetova15 <leilochkazai@gmail.com>
2024-10-08 15:37:25 +0300

    Задача "Распаковка". Цель задания: Освоить создание функций с параметрами по умолчанию и практику вызова этих функций с различным количеством аргументов.

---
+++ module_3_3.py
@@ -0,0 +1,17 @@
+# 1.Функция с параметрами по умолчанию:
+def print_params (a = 1, b = 'строка', c = True):
+    print (a, b, c)
+print_params()                      # вызов функции без присвоения аргументов
+print_params(15, True, 30) # вызов функции с разным количеством аргументов
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+# 2.Распаковка параметров:
+values_list = [1, 'Привет', False]
+values_dict = {'a' : 1, 'b' : 'Привет', 'c' : False}
+print_params(*values_list)
+print_params(**values_dict)
+
+# 3.Распаковка + отдельные параметры:
+values_list_2 = [18, 'Привет']
+print_params(*values_list_2, 42)
