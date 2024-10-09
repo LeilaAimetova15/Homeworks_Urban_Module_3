15ee7a60401bb0e13826f4510eefc17288d17c32
LeilaAimetova15 <leilochkazai@gmail.com>
2024-10-09 23:23:12 +0300

    Задача "Однокоренные": Напишите функцию single_root_words, которая принимает одно обязательное слово в параметр root_word, а далее неограниченную последовательность в параметр *other_words. Функция должна составить новый список same_words только из тех слов списка other_words, которые содержат root_word или наоборот root_word содержит одно из этих слов. После вернуть список same_words в качестве результата своей работы.

--- module_3_1.py
+++ module_3_1.py
@@ -7,7 +7,7 @@ def string_info (string) :
     return (len(string), string.upper(), string.lower())
 def is_contains (string, list_to_search) :
     count_calls()
-    return #пока не совсем поняла как прописать условие: возвращает True, если строка находится в этом списке, False - если отсутствует. Регистром строки при проверке пренебречь: UrbaN ~ URBAN.
+    return string.upper() in [i.upper() for i in list_to_search]
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
